f76db9efd7165e40d747c2cf35bfba048a7dde60
Insaf <safar_30@mail.ru>
2018-10-04 22:29:17 +0300

    На проверку

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-'use strict' ;
+"use strict";
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-
-
-
-console.log(second);
+let money = +prompt("Ваш бюджет на месяц?",'');
+console.log(money);
+let time = prompt("Введите дату в формате YYYY-MM-DD",'');
+console.log(time);
+let appData = {
+    Bank: money,
+    timeData: time,
+    expenses: {
+        "ответ на первый вопрос" : "ответ на второй вопрос"
+    },
+    optionalExpenses: {},
+    income: {},
+    savings: false
+};
+appData.expenses.ff = prompt("Введите обязательную статью расходов в этом месяце",'');
+console.log(appData.expenses.ff);
+appData.expenses.fs = +prompt("Во сколько обойдется?",'');
+console.log(appData.expenses.fs);
+appData.expenses.sf = prompt("Введите обязательную статью расходов в этом месяце",'');
+console.log(appData.expenses.sf);
+appData.expenses.ss = +prompt("Во сколько обойдется?",'');
+console.log(appData.expenses.ss);
+let Budjet;
+Budjet = (money-(appData.expenses.fs+appData.expenses.ss))/30;
+alert(Budjet);
