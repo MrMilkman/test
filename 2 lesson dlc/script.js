f81a69efb41e50afab781d488622f28e075c428f
Insaf <safar_30@mail.ru>
2018-10-06 07:50:31 +0300

    На проверку дз 2 dlc

---
+++ 2 lesson dlc/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+    a = week[0],
+    b = week[1],
+    c = week[2],
+    d = week[3],
+    e = week[4],
+    f = week[5],
+    g = week[6];
+
+document.write( a + "<br/> <br/>" );
+document.write( b + "<br/> <br/>" );
+document.write( c + "<br/> <br/>" );
+document.write( d + "<br/> <br/>" );
+document.write( e + "<br/> <br/>" );
+document.write( "<em/>" + "<b/>" + f + "<br/> <br/>" + "</em>");
+document.write( "<b/>" + g + "<br/> <br/>" );
+
+let arr = ["3823674", "123", "7531234", "5321", "42131", "3578", "098"];
+
+if (arr[0].charAt(0) == 3 || arr[0].charAt(0) == 7) {
+    console.log(arr[0]);
+};
+
+if (arr[1].charAt(0) == 3 || arr[1].charAt(0) == 7) {
+    console.log(arr[1]);
+};
+
+if (arr[2].charAt(0) == 3 || arr[2].charAt(0) == 7) {
+    console.log(arr[2]);
+};
+
+if (arr[3].charAt(0) == 3 || arr[3].charAt(0) == 7) {
+    console.log(arr[3]);
+};
+
+if (arr[4].charAt(0) == 3 || arr[4].charAt(0) == 7) {
+    console.log(arr[4]);
+};
+
+if (arr[5].charAt(0) == 3 || arr[5].charAt(0) == 7) {
+    console.log(arr[5]);
+};
+
+if (arr[6].charAt(0) == 3 || arr[6].charAt(0) == 7) {
+    console.log(arr[6]);
+};
