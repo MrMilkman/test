2a44982c33e851f299c8ea0646f0d763be3d2a36
Insaf <safar_30@mail.ru>
2018-10-22 03:19:27 +0300

    Добавил 7 задание

---
+++ 7 lesson/time/time.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function Time() {
+	let time = new Date(),
+        hour = time.getHours(),
+        min = time.getMinutes(),
+        sec = time.getSeconds();
+
+hour = nolik(hour);
+min = nolik(min);
+sec = nolik(sec);
+
+console.log(hour + ":" + min + ":" +sec);
+time = setTimeout(Time, 1000);
+}
+
+function nolik(i) {
+	if (i < 10) {	
+		i = "0" + i;
+	}
+	return i;
+}
+
+Time();
