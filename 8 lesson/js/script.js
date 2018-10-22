2d3eda663b5a29021efe1c9c44756585c593a68c
Insaf <safar_30@mail.ru>
2018-10-22 08:21:40 +0300

    добавил 8 задание

---
+++ 8 lesson/js/script.js
@@ -0,0 +1,101 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for ( let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    // Timer
+    
+	let deadline = '2018-10-23',
+        persently = Date.parse(new Date()),
+	    end = Date.parse(deadline);
+
+	if ( end <= persently ) {
+	let timer = document.getElementById('timer'),
+		hours = timer.querySelector('.hours'),
+		minutes = timer.querySelector('.minutes'),
+		seconds = timer.querySelector('.seconds');
+
+		hours.textContent = "00";
+		minutes.textContent = "00";
+		seconds.textContent = "00";
+
+	} else {
+
+		function getTimerRemaining(endtime) {
+			let t = Date.parse(endtime) - Date.parse(new Date()),
+			    seconds = Math.floor((t/1000) % 60),
+			    minutes = Math.floor((t/1000/60) % 60),
+			    hours = Math.floor((t/(1000*60*60)));
+
+			return {
+				'total': t,
+				'hours': hours,
+				'minutes': minutes,
+				'seconds': seconds
+			};
+		}
+
+		function setClock(id, endtime) {
+			let timer = document.getElementById(id),
+				hours = timer.querySelector('.hours'),
+				minutes = timer.querySelector('.minutes'),
+				seconds = timer.querySelector('.seconds');
+
+			function updateClock() {
+				let t = getTimerRemaining(endtime);
+				    hours.innerHTML = nolik(t.hours);
+				    minutes.innerHTML = nolik(t.minutes);
+				    seconds.innerHTML = nolik(t.seconds);
+
+				if (t.total <= 0) {
+					clearInterval(timeInterval);
+				}
+			}
+
+
+			updateClock();
+			let timeInterval = setInterval(updateClock, 1000);
+		}
+
+		function nolik(i) {
+			if ( i < 10 ) {	
+				i = "0" + i;
+			}
+			return i;
+		}
+		setClock('timer', deadline);
+	}
+});
+
+
