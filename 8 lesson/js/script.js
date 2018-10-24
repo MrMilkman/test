8eaf0d469d5e3708657f6fa3bda991b3d1face48
Insaf <safar_30@mail.ru>
2018-10-24 10:40:54 +0300

    Добавил 9 задание

--- 8 lesson/js/script.js
+++ 8 lesson/js/script.js
@@ -89,24 +89,4 @@ window.addEventListener('DOMContentLoaded', function() {
 			}
 		}
 		setClock('timer', deadline);
-
-
-
-	//Modal
-
-	let more = document.querySelector('.more'),
-		overlay = document.querySelector('.overlay'),
-		close = document.querySelector('.popup-close');
-	
-	more.addEventListener('click', function() {
-		overlay.style.display = 'block';
-		this.classList.add('more-splash');
-		document.body.style.overflow = 'hidden';
-	});
-
-	close.addEventListener('click', function() {
-		overlay.style.display = 'none';
-		more.classList.add('remove-splash');
-		document.body.style.overflow = '';
-	});
 });
